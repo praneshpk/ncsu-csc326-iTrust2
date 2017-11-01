5fb4daddda488ada917523b14172d2005f3645a7
ksmitch3 <ksmitch@ncsu.edu>
2017-11-01 15:02:09 -0400

    Began cucumber testing/fixed minor bugs in addPrescription.html

--- iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/ITRunner.java
+++ iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/ITRunner.java
@@ -5,8 +5,8 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@RunWith ( Cucumber.class )
-@CucumberOptions ( features = "src/test/resources/edu/ncsu/csc/itrust/cucumber/" )
+@RunWith ( Cucumber.class ) //changed path for testing - remove BlackBox.feature to test everything
+@CucumberOptions ( features = "src/test/resources/edu/ncsu/csc/itrust/cucumber/BlackBox.feature" )
 public class ITRunner {
-
+	
 }
